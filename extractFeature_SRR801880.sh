dad01d96ef5ce64c88646b8c58d4dd723523e198
maobing <ice82444@qq.com>
2015-01-14 19:32:02 -0500

    for backup purpose

--- extractFeature_SRR801880.sh
+++
@@ -1,9 +0,0 @@
-# ./extractFeature -i ../DNaseFlash_SRR801880 -t ../CMYCData_narrowPeak_2cat.txt -o ../CMYCOutputFeature_2cat_SRR801880.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801880 -t ../CTCFData_narrowPeak_2cat.txt -o ../CTCFOutputFeature_2cat_SRR801880.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801880 -t ../E2FData_narrowPeak_2cat.txt -o ../E2FOutputFeature_2cat_SRR801880.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801880 -t ../EGR1Data_narrowPeak_2cat.txt -o ../EGR1OutputFeature_2cat_SRR801880.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801880 -t ../GABPData_narrowPeak_2cat.txt -o ../GABPOutputFeature_2cat_SRR801880.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801880 -t ../NRSFData_narrowPeak_2cat.txt -o ../NRSFOutputFeature_2cat_SRR801880.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801880 -t ../SRFData_narrowPeak_2cat.txt -o ../SRFOutputFeature_2cat_SRR801880.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801880 -t ../USF1Data_narrowPeak_2cat.txt -o ../USF1OutputFeature_2cat_SRR801880.txt -p ../param
-
--- extractFeature_SRR801881.sh
+++
@@ -1,9 +0,0 @@
-# ./extractFeature -i ../DNaseFlash_SRR801881 -t ../CMYCData_narrowPeak_2cat.txt -o ../CMYCOutputFeature_2cat_SRR801881.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801881 -t ../CTCFData_narrowPeak_2cat.txt -o ../CTCFOutputFeature_2cat_SRR801881.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801881 -t ../E2FData_narrowPeak_2cat.txt -o ../E2FOutputFeature_2cat_SRR801881.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801881 -t ../EGR1Data_narrowPeak_2cat.txt -o ../EGR1OutputFeature_2cat_SRR801881.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801881 -t ../GABPData_narrowPeak_2cat.txt -o ../GABPOutputFeature_2cat_SRR801881.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801881 -t ../NRSFData_narrowPeak_2cat.txt -o ../NRSFOutputFeature_2cat_SRR801881.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801881 -t ../SRFData_narrowPeak_2cat.txt -o ../SRFOutputFeature_2cat_SRR801881.txt -p ../param
-./extractFeature -i ../DNaseFlash_SRR801881 -t ../USF1Data_narrowPeak_2cat.txt -o ../USF1OutputFeature_2cat_SRR801881.txt -p ../param
-
